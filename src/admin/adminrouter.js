22762ebc012e0bbfde767257cd3f8042081a8252
Lseoksee <da864268@naver.com>
2023-07-22 15:17:07 +0900

    안쓰는 파일 삭제

--- src/admin/adminrouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var router = express.Router();
-router.use(express.static(__dirname));
-router.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
-exports.default = router;
