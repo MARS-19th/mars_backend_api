5f0dfe71312b9718411f897c0dd5a9fb10b14c96
Lseoksee <da864268@naver.com>
2023-10-11 00:56:21 +0900

    firebase-admin 으로 테스트

--- test/reqtest.ts
+++ test/reqtest.ts
@@ -1,4 +1,6 @@
 import * as fs from "fs";
+import * as firebase from "firebase-admin";
+
 const serverset = JSON.parse(fs.readFileSync("bin/server.json", "utf-8"));
 
 async function gettest(url: string) {
@@ -11,7 +13,7 @@ async function gettest(url: string) {
 async function settest(url: string) {
     const body = {
         user_name: "관리자1",
-        object_id: 8
+        object_id: 8,
     };
 
     const response = await fetch(`http://korseok.kro.kr/api/${url}`, {
@@ -26,30 +28,31 @@ async function settest(url: string) {
     console.log(res);
 }
 
-async function chatreq() {
-    const token = "ya29.a0AfB_byD1c0gZbLcku1kZmBET-akn3OHwK1ksO1H4GrbyosBp7-bb1f9PXjzvpfus0U_KEgWudCpckOOh9HypkptRRY8c70aZReNlBAZ-FfbeYgceeSsfmeIdSHqifR3kpfgCLGG8JzIFK08yI-7YTqKO_Mbx3Fxndzt4aCgYKAQoSARMSFQGOcNnCaQY5pBHjEHCvD3O3DkZWhA0171"
-
-    const body = {
-        message: {
-            token: "chu1zDfyQJS5gvL9b0CIT-:APA91bGMCVSHbkLBlURPBhpOuL1EpVkiBVxalEGT8I9RVpGusqDSEvfFPgQ8_MCarixJEKSyclV2Gk5GEaLxV0I_PXO2mS-TTo7F1U-VI6rWBH5jKEaodc5-BynH5nkWMjO4praii_hw",
-            notification: {
-                body: "hi",
-                title: "sadada"
-            }
-        }
-    };
-
-    const response = await fetch(`https://fcm.googleapis.com/v1/projects/growverse-21715/messages:send`, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer "+ token,
-        },
-        body: JSON.stringify(body),
+function chatreq() {
+    firebase.initializeApp({
+        credential: firebase.credential.cert(serverset.fcmkey),
     });
 
-    const res = await response.text();
-    console.log(res);
+    let target_token = `chu1zDfyQJS5gvL9b0CIT-:APA91bGMCVSHbkLBlURPBhpOuL1EpVkiBVxalEGT8I9RVpGusqDSEvfFPgQ8_MCarixJEKSyclV2Gk5GEaLxV0I_PXO2mS-TTo7F1U-VI6rWBH5jKEaodc5-BynH5nkWMjO4praii_hw`;
+    //target_token은 푸시 메시지를 받을 디바이스의 토큰값입니다
+
+    let message = {
+        notification: {
+            title: "테스트 데이터 발송",
+            body: "데이터가 잘 가나요?",
+        },
+        token: target_token,
+    };
+
+    firebase
+        .messaging()
+        .send(message)
+        .then((response) => {
+            console.log("Successfully sent message: : ", response);
+        })
+        .catch((err) => {
+            console.log("Error Sending message!!! : ", err);
+        });
 }
 
 chatreq();
