f56e3584b9c0bbbec9ef23bf5b18bba1c25de1ac
Lseoksee <da864268@naver.com>
2023-10-11 00:23:17 +0900

    채팅 테스트

--- test/reqtest.ts
+++ test/reqtest.ts
@@ -26,4 +26,30 @@ async function settest(url: string) {
     console.log(res);
 }
 
-settest("setuserinventory");
+async function chatreq() {
+    const token = "ya29.a0AfB_byD1c0gZbLcku1kZmBET-akn3OHwK1ksO1H4GrbyosBp7-bb1f9PXjzvpfus0U_KEgWudCpckOOh9HypkptRRY8c70aZReNlBAZ-FfbeYgceeSsfmeIdSHqifR3kpfgCLGG8JzIFK08yI-7YTqKO_Mbx3Fxndzt4aCgYKAQoSARMSFQGOcNnCaQY5pBHjEHCvD3O3DkZWhA0171"
+
+    const body = {
+        message: {
+            token: "chu1zDfyQJS5gvL9b0CIT-:APA91bGMCVSHbkLBlURPBhpOuL1EpVkiBVxalEGT8I9RVpGusqDSEvfFPgQ8_MCarixJEKSyclV2Gk5GEaLxV0I_PXO2mS-TTo7F1U-VI6rWBH5jKEaodc5-BynH5nkWMjO4praii_hw",
+            notification: {
+                body: "hi",
+                title: "sadada"
+            }
+        }
+    };
+
+    const response = await fetch(`https://fcm.googleapis.com/v1/projects/growverse-21715/messages:send`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer "+ token,
+        },
+        body: JSON.stringify(body),
+    });
+
+    const res = await response.text();
+    console.log(res);
+}
+
+chatreq();
